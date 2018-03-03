f26d11de029c052e440d147fd9d1de6b4982eaa3
Dmytriy Povaliaiev <dmytro.povalyaev@gmail.com>
2018-03-04 01:14:34 +0200

    Add runner & setup benchmark for Example A

---
+++ runner.py
@@ -0,0 +1,23 @@
+import time
+from solution import solve
+
+def run_benchmark(example_name, input_dataset, output_file):
+    print('Processing Example {name}'.format(name = example_name))
+
+    start_time = time.perf_counter()
+    solve(input_dataset, output_file)
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+
+    print('Example {name} took {elapsed_time}s'.format(
+        name = example_name,
+        elapsed_time = elapsed_time
+        ))
+
+
+def main():
+    run_benchmark('A', 'input_data/a_example.in', 'output_data/a_example.out')
+
+
+if __name__ == '__main__':
+    main()
