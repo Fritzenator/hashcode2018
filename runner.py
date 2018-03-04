01f374e24cd5c80324cd6a47b9a15bb8c22818ab
Dmytriy Povaliaiev <dmytro.povalyaev@gmail.com>
2018-03-04 02:01:48 +0200

    Add Dataset B runner

--- runner.py
+++ runner.py
@@ -17,6 +17,7 @@ def run_benchmark(example_name, input_dataset, output_file):
 
 def main():
     run_benchmark('A', 'input_data/a_example.in', 'output_data/a_example.out')
+    run_benchmark('B', 'input_data/b_should_be_easy.in', 'output_data/b_should_be_easy.out')
 
 
 if __name__ == '__main__':
